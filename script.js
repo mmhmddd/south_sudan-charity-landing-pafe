cc15ae31523280f2c108a8c4de0bc78fcd3a8d52
mmhmddd <mohamed.m.mahmoud@gmail.com>
2025-04-12 17:23:34 +0200

    Your update message here

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
 
-  document.addEventListener('DOMContentLoaded', function () {
-    // Initialize the map centered on South Sudan (approx. coordinates: 4.85, 31.58)
-    var map = L.map('map').setView([4.85, 31.58], 6);
+document.addEventListener('DOMContentLoaded', function () {
+  // Updated coordinates for Malakia, Juba
+  var map = L.map('map').setView([4.8419, 31.5908], 15);
 
-    // Add OpenStreetMap tiles with a custom style
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      maxZoom: 18,
-    }).addTo(map);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 18,
+  }).addTo(map);
 
-    // Custom marker icon
-    var customIcon = L.divIcon({
-      className: 'custom-marker',
-      iconSize: [12, 12],
-    });
-
-    // Add a marker for South Sudan (e.g., Juba)
-    L.marker([4.85, 31.58], { icon: customIcon })
-      .addTo(map)
-      .bindPopup('<b>Our Work in South Sudan</b><br>Supporting flood-affected communities.')
-      .openPopup();
+  var customIcon = L.divIcon({
+    className: 'custom-marker',
+    iconSize: [12, 12],
   });
 
+  L.marker([4.8419, 31.5908], { icon: customIcon })
+    .addTo(map)
+    .bindPopup('<b>Malakia, Juba</b><br>Our presence here supports local communities.')
+    .openPopup();
+});
 
 
   // Function to toggle navbar transparency on scroll
